7a14364d229028b1c7ae64b25d5f59220d41aaf7
pisonpeakgroup <pisonpeakgroup@gmail.com>
2017-05-24 17:01:02 +0100

    Update and rename strain-spec to strain-spec.js

---
+++ spec/strain-spec.js
@@ -0,0 +1,15 @@
+describe("MathUtils", function() {
+    var calc;
+ 
+    //This will be called before running each spec
+    beforeEach(function() {
+        calc = new MathUtils();
+    });
+ 
+    describe("when calc is used to peform basic math operations", function(){
+         
+       
+        });
+         
+    });
+});
